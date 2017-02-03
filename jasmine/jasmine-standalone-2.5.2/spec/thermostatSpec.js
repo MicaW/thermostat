6601e952f36799c7fad24f9b25ba1cb5246dafe9
Jack <92jackhenderson@gmail.com>
2017-02-03 14:04:24 +0000

    tests all passing

--- jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -11,27 +11,63 @@ describe('Thermostat', function(){
       expect(thermostat.temperature).toBe(20)
     });
 
-    // it("increase the temperature with an up function", function() {
-    //   thermostat.up(5);
-    //   expect(thermostat.temperature).toBe(25);
-    // });
-    //
+    it("be in power save mode by default", function() {
+      expect(thermostat._isPowerSave).toBe(true)
+    })
+
+    it("increase the temperature with an up function", function() {
+      thermostat.up(5);
+      expect(thermostat.temperature).toBe(25);
+    });
+
     it("decrease the temperature with an down function", function() {
       thermostat.down(5);
       expect(thermostat.temperature).toBe(15);
     });
 
-    // it("have a minimum temperature of 10 degrees", function() {
-    //   expect(function() {thermostat.down(20)}).toThrow("minimum temperature is 10");
-    //   expect(thermostat.temperature).toBe(10);
-    // });
-    //
-    // it("limit the temperature at 25 when power saving mode is on", function() {
-    //   thermostat.powerSaveOn();
-    //   expect(function() {thermostat.up(6)}).toThrow("maximum temperature is 25 in power save mode");
-    //   expect(thermostat.temperature).toBe(25);
-    // });
+    it("have a minimum temperature of 10 degrees", function() {
+      expect(function() {thermostat.down(20)}).toThrow("minimum temperature is 10");
+      expect(thermostat.temperature).toBe(10);
+    });
 
+    it("limit the temperature at 25 when power saving mode is on", function() {
+      thermostat.powerSaveOn();
+      expect(function() {thermostat.up(6)}).toThrow("maximum temperature is 25 in power save mode");
+      expect(thermostat.temperature).toBe(25);
+    });
+
+    it("have a power save function that can be turned on", function () {
+      thermostat.powerSaveOn()
+      expect(thermostat._isPowerSave).toBe(true)
+    });
+
+    it("have a power save function that can be turned off", function () {
+      thermostat.powerSaveOff()
+      expect(thermostat._isPowerSave).toBe(false)
+      expect(thermostat.maxTemp).toBe(32)
+    });
+
+    it("reset temperature to 20 degrees", function() {
+      thermostat.reset()
+      expect(thermostat.temperature).toBe(20)
+    });
+
+    describe("#usage", function(){
+  it("above 25 is high", function(){
+    thermostat.temperature = 25
+    expect(thermostat.usage()).toEqual('High')
+  });
+
+  it("between 18 and 25", function(){
+    thermostat.temperature = 20
+    expect(thermostat.usage()).toEqual('Medium')
+  });
+
+  it("below 18 is low", function(){
+    thermostat.temperature = 17
+    expect(thermostat.usage()).toEqual('Low')
+  });
+});
 
   });
 
--- jasmine/jasmine-standalone-2.5.2/src/thermostat.js
+++ jasmine/jasmine-standalone-2.5.2/src/thermostat.js
@@ -1,32 +1,51 @@
-function Thermostat() {
+function Thermostat () {
   this.temperature = 20
-  this.powerSave = true
-  // this.maxTemp = 25
-  // this.minTemp = 10
+  this.minTemp = 10
+  this.maxTemp = 25
+  this._isPowerSave = true
 };
-  //
-  // Thermostat.prototype.up = function(number) {
-  //   if((this.temperature + number) > this.maxTemp)
-  //     { this.temperature = this.maxTemp
-  //     throw("maximum temperature is" + this.maxTemp + " in power save mode")
-  //     };
-  //  else this.temperature += number
-  // };
-  //
-  Thermostat.prototype.down = function(number) {
-    if((this.temperature - number) < 10)
-      {this.temperature = 10 ;
+
+Thermostat.prototype.up = function(number = 1){
+  if (this.maxTempExceeded(number)){
+    this.temperature = this.maxTemp;
+      throw("maximum temperature is " + this.maxTemp + " in power save mode")
+    }
+  else
+  this.temperature += number;
+};
+
+Thermostat.prototype.down = function(number = 1){
+  if ((this.temperature - number) < this.minTemp)
+    {this.temperature = this.minTemp;
       throw("minimum temperature is 10")
-    };
-    else this.temperature -= number
-  };
+    }
+  else this.temperature -= number
+};
 
-  Thermostat.prototype.powerSaveOn = function() {
-    this.powerSave = true
-    // this.maxTemp = 25
-  };
+Thermostat.prototype.powerSaveOn = function(){
+    this.maxTemp = 25;
+    this._isPowerSave = true
+};
 
-  Thermostat.prototype.powerSaveOff = function() {
-    this.powerSave = false
-    // this.maxTemp = 32
+Thermostat.prototype.powerSaveOff = function(){
+    this.maxTemp = 32;
+    this._isPowerSave = false
+};
+
+Thermostat.prototype.maxTempExceeded = function(number){
+  return ((this.temperature + number) > this.maxTemp)
+};
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20
+};
+
+Thermostat.prototype.usage = function() {
+  if (this.temperature >= 25){
+    return "High"
+  } else if (this.temperature < 18) {
+    return "Low"
+  } else {
+    return "Medium"
   };
+};
